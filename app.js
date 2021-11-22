e1e7eacab42ced5696a00419c50ef201a35514bd
Ricky Leung <ricxx0320@gmail.com>
2021-11-21 16:27:38 -0800

    modified code from snapshot

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
+const profileDataArgs = process.argv.slice(2);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-// Notice the lack of parentheses around the `profileDataArr` parameter?
 const printProfileData = profileDataArr => {
     // This...
     for (let i=0; i < profileDataArr.length; i += 1) {
